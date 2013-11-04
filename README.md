084078da538d5e3fa7c6d75fa8bc95368267f86c
Zoltan <zdebre@gmail.com>
2013-11-04 01:37:05 +0000

    clean up

---
+++ README.md
@@ -0,0 +1,4 @@
+WEB APIs
+===========================
+
+Different Web and Social Network API solutions and practices.
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-DemoApp::Application.config.secret_token = 'b6206573676291418f478d175c5effb23227687b2f1f9c696af3090dd2d5b47097b3a03e57440523334ab437cc291a5aca5a6197d5bea8a40bc2e4bd18256332'
