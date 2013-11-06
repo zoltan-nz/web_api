00aef2e5470a8ee8d1f0b2af756803858311b076
Zoltan <zdebre@gmail.com>
2013-11-06 02:21:42 +0000

    twitter login working with twitteroauth, yey :)

--- simple_api_app/index.php
+++ simple_api_app/index.php
@@ -20,29 +20,30 @@ include (LAYOUT_PATH.'/header.php');
                 <button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas">Toggle nav</button>
             </p>
             <div class="jumbotron">
-                <h1>Welcome</h1>
-                <p>Please login with one of the following option.</p>
+                <h1>Laravel Information Site</h1>
+                <p>This is a site about Laravel framework. All data downloaded from social networks.</p>
+                <p>If you want to interact, please login with one of the following options.</p>
                 <div class="row">
-                    <div class="col-md-4 col-sm-4"><a class="btn btn-lg btn-primary btn-block" href="facebook_login.php">Facebook</a></div>
-                    <div class="col-md-4 col-sm-4"><a class="btn btn-lg btn-info btn-block" href="twitter_login.php">Twitter</a></div>
-                    <div class="col-md-4 col-sm-4"><a class="btn btn-lg btn-danger btn-block" href="google_login.php">Google+</a></div>
+                    <div class="col-md-4 col-sm-4"><a class="btn btn-lg btn-primary btn-block" href="facebook_login.php">Login with Facebook</a></div>
+                    <div class="col-md-4 col-sm-4"><a class="btn btn-lg btn-info btn-block" href="twitter_login.php">Login with Twitter</a></div>
+                    <div class="col-md-4 col-sm-4"><a class="btn btn-lg btn-danger btn-block" href="google_login.php">Lowgin with Google+</a></div>
                 </div>
             </div>
             <div class="row">
                 <div class="col-6 col-sm-6 col-lg-4">
-                    <h2>Heading</h2>
-                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                    <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+                    <h2>Facebook</h2>
+                    <p>In this section you can play with Facebook buttons</p>
+
                 </div><!--/span-->
                 <div class="col-6 col-sm-6 col-lg-4">
-                    <h2>Heading</h2>
-                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                    <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+                    <h2>Twitter</h2>
+                    <p>Twitter related informations</p>
+                    <p><?php print_r($_SESSION['twitter_results']->name) ?></p>
+
                 </div><!--/span-->
                 <div class="col-6 col-sm-6 col-lg-4">
-                    <h2>Heading</h2>
-                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                    <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+                    <h2>Google+</h2>
+                    <p>Everything from Google+</p>
                 </div><!--/span-->
                 <div class="col-6 col-sm-6 col-lg-4">
                     <h2>Heading</h2>
@@ -62,20 +63,20 @@ include (LAYOUT_PATH.'/header.php');
             </div><!--/row-->
         </div><!--/span-->
 
-        <div class="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar" role="navigation">
-            <div class="list-group">
-                <a href="#" class="list-group-item active">Link</a>
-                <a href="#" class="list-group-item">Link</a>
-                <a href="#" class="list-group-item">Link</a>
-                <a href="#" class="list-group-item">Link</a>
-                <a href="#" class="list-group-item">Link</a>
-                <a href="#" class="list-group-item">Link</a>
-                <a href="#" class="list-group-item">Link</a>
-                <a href="#" class="list-group-item">Link</a>
-                <a href="#" class="list-group-item">Link</a>
-                <a href="#" class="list-group-item">Link</a>
-            </div>
-        </div><!--/span-->
+<!--        <div class="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar" role="navigation">-->
+<!--            <div class="list-group">-->
+<!--                <a href="#" class="list-group-item active">Link</a>-->
+<!--                <a href="#" class="list-group-item">Link</a>-->
+<!--                <a href="#" class="list-group-item">Link</a>-->
+<!--                <a href="#" class="list-group-item">Link</a>-->
+<!--                <a href="#" class="list-group-item">Link</a>-->
+<!--                <a href="#" class="list-group-item">Link</a>-->
+<!--                <a href="#" class="list-group-item">Link</a>-->
+<!--                <a href="#" class="list-group-item">Link</a>-->
+<!--                <a href="#" class="list-group-item">Link</a>-->
+<!--                <a href="#" class="list-group-item">Link</a>-->
+<!--            </div>-->
+<!--        </div><!--/span-->-->
     </div><!--/row-->
 
 
---
+++ simple_api_app/twitter_callback.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @file
+ * Take the user when they return from Twitter. Get access tokens.
+ * Verify credentials and redirect to based on response from Twitter.
+ */
+
+error_reporting(-1);
+
+define("MY_INC_CODE", 1);
+define("LAYOUT_PATH", "layout");
+define("LIBRARY_PATH", "lib");
+
+/* Start session and load lib */
+session_start();
+require(LIBRARY_PATH . '/twitteroauth/twitteroauth/twitteroauth.php');
+require 'secret_tokens.php';
+
+/* If the oauth_token is old redirect to the connect page. */
+if (isset($_REQUEST['oauth_token']) && $_SESSION['oauth_token'] !== $_REQUEST['oauth_token']) {
+    $_SESSION['oauth_status'] = 'oldtoken';
+    header('Location: ./clearsessions.php');
+}
+
+/* Create TwitteroAuth object with app key/secret and token key/secret from default phase */
+$connection = new TwitterOAuth(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET, $_SESSION['oauth_token'], $_SESSION['oauth_token_secret']);
+
+/* Request access tokens from twitter */
+$access_token = $connection->getAccessToken($_REQUEST['oauth_verifier']);
+
+/* Save the access tokens. Normally these would be saved in a database for future use. */
+$_SESSION['access_token'] = $access_token;
+
+/* Remove no longer needed request tokens */
+unset($_SESSION['oauth_token']);
+unset($_SESSION['oauth_token_secret']);
+
+/* If HTTP response is 200 continue otherwise send to connect page to retry */
+if (200 == $connection->http_code) {
+    /* The user has been verified and the access tokens can be saved for future use */
+    $_SESSION['status'] = 'verified';
+
+    /* Get user access tokens out of the session. */
+    $access_token = $_SESSION['access_token'];
+
+    /* Create a TwitterOauth object with consumer/user tokens. */
+    $connection = new TwitterOAuth(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
+
+    $_SESSION['twitter_results'] = $connection->get('account/verify_credentials');
+
+    header('Location: ./index.php');
+} else {
+    /* Save HTTP status for error dialog on connnect page.*/
+    header('Location: ./clearsessions.php');
+}
